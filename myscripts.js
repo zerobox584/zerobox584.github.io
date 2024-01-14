d5851536c8dfcf57318ad915207e8905e33a73db
Bunny <zerobox584boxzazaza@gmail.com>
2024-01-15 00:31:39 +0700

    improt script jquery

--- myscripts.js
+++ myscripts.js
@@ -7,14 +7,16 @@ $(document).ready(function () {
         modal.find('#recipient-img').attr('src', recipient);
     })
     $('#mongoIcon').tooltip({
-        trigger: 'manual',
+        trigger: 'manual', // กำหนดให้ tooltip แสดงผลเมื่อถูกเรียกด้วย JavaScript
     });
 
+    // เพิ่ม event listener เมื่อนำนิ้วไปที่ icon
     $('#mongoIcon').on('mousedown', function () {
-        $(this).tooltip('show');
+        $(this).tooltip('show'); // แสดง tooltip เมื่อนำนิ้วไปที่ icon
     });
 
+    // เพิ่ม event listener เมื่อปล่อยนิ้ว
     $('#mongoIcon').on('mouseup', function () {
-        $(this).tooltip('hide');
+        $(this).tooltip('hide'); // ซ่อน tooltip เมื่อปล่อยนิ้ว
     });
 });
