8934cba0270a29aa0c064f40abd61b9fa3312ae4
Bunny <zerobox584boxzazaza@gmail.com>
2024-01-15 00:33:34 +0700

    before toptip

--- myscripts.js
+++ myscripts.js
@@ -6,17 +6,4 @@ $(document).ready(function () {
         var modal = $(this)
         modal.find('#recipient-img').attr('src', recipient);
     })
-    $('#mongoIcon').tooltip({
-        trigger: 'manual', // กำหนดให้ tooltip แสดงผลเมื่อถูกเรียกด้วย JavaScript
-    });
-
-    // เพิ่ม event listener เมื่อนำนิ้วไปที่ icon
-    $('#mongoIcon').on('mousedown', function () {
-        $(this).tooltip('show'); // แสดง tooltip เมื่อนำนิ้วไปที่ icon
-    });
-
-    // เพิ่ม event listener เมื่อปล่อยนิ้ว
-    $('#mongoIcon').on('mouseup', function () {
-        $(this).tooltip('hide'); // ซ่อน tooltip เมื่อปล่อยนิ้ว
-    });
 });
