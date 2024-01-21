ce6ac7e6db5ec687a1d6f5ab741e1acd554f46b8
Sitthichai <zerobox584boxzazaza@gmail.com>
2024-01-21 23:40:30 +0700

    change image and add row image

--- myscripts.js
+++ myscripts.js
@@ -6,12 +6,32 @@ $(document).ready(function () {
         var modal = $(this)
         modal.find('#recipient-img').attr('src', recipient);
     })
-    $('#resultModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var text = button.data('text');
-        var image = button.data('src')
-        var modal = $(this)
-        modal.find('#recipient-text').text(text);
-        modal.find('#recipient-img').attr('src', image);
-    });
+    // $('#resultModal').on('show.bs.modal', function (event) {
+    //     var button = $(event.relatedTarget);
+    //     var text = button.data('text');
+    //     var image = button.data('src')
+    //     var modal = $(this)
+    //     modal.find('#recipient-text').text(text);
+    //     modal.find('#recipient-img').attr('src', image);
+    // });
+
+    var loader = $("#loader");
+
+    function loadNow(opacity) {
+        if (opacity <= 0) {
+            displayContent();
+        } else {
+            loader.css("opacity", opacity);
+            setTimeout(function () {
+                loadNow(opacity - 0.04);
+            }, 50);
+        }
+    }
+
+    function displayContent() {
+        loader.hide();
+        $("#content").show();
+    }
+
+    loadNow(1);
 });
